455c06beb273447ab10754d15ca7ba1751cd4c95
moonishkale <manishkhadgi123@gmail.com>
2023-01-08 09:46:59 +1100

    backend

---
+++ server.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+import cors from "cors";
+import morgan from "morgan";
+
+//connect DB
+import { connectDB } from "./src/config/dbConfig.js";
+connectDB();
+
+//middlwares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+//API routers
+
+//uncaugh router hanlder   page not found hanlde garna laiX
+app.use("*", (req, res) => {
+  const error = {
+    errorCode: 404,
+    message: "Requested resouces not found",
+  };
+});
+
+//global error hanlder   page 500 server hanlde garna lai
+app.use((error, req, res, next) => {
+  try {
+    console.log(error.message);
+    const errorCode = error.errorCode || 500;
+
+    res.status(errorCode).json({
+      status: "error",
+      message: error.message,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+//Run the server
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`your server us running at http://localhost:${PORT}`);
+});
